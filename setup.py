c3a540468bf492bafa71e1224799d0d064bbf0a1
Neptun <kgsvitalka9510@gmail.com>
2026-07-14 10:42:35 +0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="security-scanner",
+    version="1.0.0",
+    packages=find_packages(),
+    author="Developer",
+    description="Python security scanner",
+    python_requires=">=3.8",
+)
